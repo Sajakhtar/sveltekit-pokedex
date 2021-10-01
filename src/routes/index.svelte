9265bf60b1e9a3e24e8b2c03180d2d6b754316f4
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-10-01 18:33:39 +0400

    index - update h1, remove paragraph, add style h1 col,  add script to console log

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  console.log('hello pokemon')
+</script>
+
+<h1>SvelteKit Pokedex</h1>
+
+
+<style>
+  h1 {
+    color: cornflowerblue;
+  }
+</style>
