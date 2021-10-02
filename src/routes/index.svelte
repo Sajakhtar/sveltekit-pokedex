faa5c170efa062d40f2d6a46b2290f2164d6a910
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-10-02 11:27:13 +0400

    style pokeman card and add image

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -10,6 +10,9 @@
 
 <h1 class="text-4xl text-center my-8 uppercase">SvelteKit Pokedex</h1>
 
-{#each $pokemon as pokeman}
-  <PokemanCard pokeman={pokeman} />
-{/each}
+
+<div class="py-4 grid gap-4 md:grid-cols-2 grid-cols-1">
+  {#each $pokemon as pokeman}
+    <PokemanCard pokeman={pokeman} />
+  {/each}
+</div>
