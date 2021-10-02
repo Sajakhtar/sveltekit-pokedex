523ce4bd76a04a8eec130dcd2e7f30779e7c1510
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-10-02 11:08:30 +0400

    add tailwind css styling to index and about

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -3,6 +3,6 @@
   <title>About | SvelteKit Pokedex</title>
 </svelte:head>
 
-<h1>About Pokedex</h1>
+<h1 class="text-4xl text-center my-8 uppercase">About Pokedex</h1>
 
 <p>This is sample SveltKit project to demonstrate the capabilities of SvelteKit. The project is a Pokedex and uses the Pokemon API.</p>
