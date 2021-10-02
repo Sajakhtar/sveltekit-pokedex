774bdf1c01d3a663cd55ca8c34fa7a1b13c766d1
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-10-02 14:19:18 +0400

    serverside rendering pokemon routes dynamically with fetch using script module, refactor index to fetch using script module and nnot via stores

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -5,4 +5,4 @@
 
 <h1 class="text-4xl text-center my-8 uppercase">About Pokedex</h1>
 
-<p>This is sample SveltKit project to demonstrate the capabilities of SvelteKit. The project is a Pokedex and uses the Pokemon API.</p>
+<p class="text-center">This is sample SveltKit project to demonstrate the capabilities of SvelteKit. The project is a Pokedex and uses the Pokemon API.</p>
