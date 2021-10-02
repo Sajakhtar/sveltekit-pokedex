d977e772d03ef5cf944aafb488c8308799a0b3e6
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-10-02 12:15:15 +0400

    add search input in index and bind for reactivity

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,17 @@
 <script>
-  import { pokemon } from "../stores/pokestore"
-  import PokemanCard from "../components/pokemanCard.svelte"
+  import { pokemon } from "../stores/pokestore";
+  import PokemanCard from "../components/pokemanCard.svelte";
+
+  let searchTerm = "";
+  let filteredPokemon = [];
+
+  $:{
+    if(searchTerm) {
+      filteredPokemon = $pokemon.filter(pokeman => pokeman.name.toLowerCase().includes(searchTerm.toLowerCase()));
+    } else {
+      filteredPokemon = [...$pokemon]
+    }
+  }
 </script>
 
 
@@ -10,9 +21,10 @@
 
 <h1 class="text-4xl text-center my-8 uppercase">SvelteKit Pokedex</h1>
 
+<input class="w-full rounded-md text-lg p-4 border-2 border-gray-200" type="text" bind:value={searchTerm} placeholder="Search Pokemon">
 
 <div class="py-4 grid gap-4 md:grid-cols-2 grid-cols-1">
-  {#each $pokemon as pokeman}
+  {#each filteredPokemon as pokeman}
     <PokemanCard pokeman={pokeman} />
   {/each}
 </div>
